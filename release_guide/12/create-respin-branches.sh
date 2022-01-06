366a2a658bec79eff5958e85242860940d7d176f
Jeff MAURY <jmaury@redhat.com>
2022-01-06 13:59:25 +0100

    Add jbosstools-build to respin
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- release_guide/12/create-respin-branches.sh
+++ release_guide/12/create-respin-branches.sh
@@ -1,9 +1,9 @@
 versionWithRespin_jbt_PREV=4.21.0.Final
-versionWithRespin_jbt=4.21.2.GA
+versionWithRespin_jbt=4.21.2.Final
 versionWithRespin_ds_PREV=12.21.0.GA
 versionWithRespin_ds=12.21.2.GA
 
-for i in base central discovery forge fuse hibernate javaee jst livereload openshift quarkus server vpe webservices
+for i in base build central discovery forge fuse hibernate javaee jst livereload openshift quarkus server vpe webservices
 do
 	git clone git@github.com:jbosstools/jbosstools-$i -b jbosstools-${versionWithRespin_jbt_PREV}
 	cd jbosstools-$i
@@ -25,6 +25,6 @@ git push origin ${versionWithRespin_jbt}:${versionWithRespin_jbt}
 cd ..
 git clone git@github.com:jbdevstudio/jbdevstudio-product -b jbdevstudio-${versionWithRespin_ds_PREV}
 cd jbdevstudio-product
-git checkout -b ${versionWithRespin_ds}
-git push origin ${versionWithRespin_ds}:${versionWithRespin_ds}
+git checkout -b ${versionWithRespin_jbt}
+git push origin ${versionWithRespin_jbt}:${versionWithRespin_jbt}
 cd ..
